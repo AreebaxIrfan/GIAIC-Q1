214d74911d4855231356b41031c24c4cb9c89a95
Areeba Irfan <161799302+Areeba-irfan-Github-account@users.noreply.github.com>
2024-08-01 23:11:56 +0500

    Class_3

---
+++ main.js
@@ -0,0 +1,20 @@
+"use strict";
+//DATA TYPE//TS 
+//permitive data type
+let myName = "Areeba Irfan";
+// myName = 240;///infer
+myName = "The Cat";
+let myAge = 17;
+// myAge = "dist" //infer
+myAge = 35.2466;
+console.log(myAge.toFixed(2)); //
+// let isJobless: boolean = false;
+// // isJobless = 35  ///infere data ko dekh kar data type karta ha..
+// isJobless = true;
+console.log(myName.toUpperCase()); //THE WOLF
+//undefined
+let myplan;
+console.log(myplan); //undefined
+//null
+let myCar = null;
+console.log(myCar);
---
+++ main.ts
@@ -0,0 +1,23 @@
+//DATA TYPE//TS 
+
+//permitive data type
+let myName:string = "Areeba Irfan";
+// myName = 240;///infer
+myName ="The Cat";
+
+let myAge: number  = 17;
+// myAge = "dist" //infer
+myAge = 35.2466;
+console.log(myAge.toFixed(2)); //round off the number ..
+
+// let isJobless: boolean = false;
+// // isJobless = 35  ///infere data ko dekh kar data type karta ha..
+// isJobless = true;
+
+console.log ( myName.toUpperCase()); //THE WOLF
+//undefined
+let myplan;
+console.log(myplan); //undefined
+ //null
+ let myCar: null = null
+ console.log(myCar)
