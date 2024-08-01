360c71d836cb797453f13b7dde8f6907248186f3
Areeba Irfan <161799302+Areeba-irfan-Github-account@users.noreply.github.com>
2024-08-01 23:14:23 +0500

    Class_6

--- main.js
+++ main.js
@@ -1,16 +1,33 @@
-//kal class huogi..
-var isRainy = false;
-if (isRainy) {
-    console.log("Monday class is Off");
-}
-else {
-    console.log("there is no off");
-}
-console.log("--------------------------------------");
-var isCarPunctured = "No";
-if (isCarPunctured === "Yes") {
-    console.log("I will be late for the class");
-}
-else {
-    console.log("I will be on the time");
-}
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//Array 
+const shoppingLists = ["Bread", 'Milk', 'Butter'];
+console.log(shoppingLists);
+console.log("-----------------");
+const shoppingList = ["Bread", 'Milk', 'Butter', 3, 5];
+console.log(shoppingList);
+console.log("-----------------");
+//uniontype
+const item = ["rose", "tulip", "jasmine", 2, 4];
+console.log(item);
+//Methods of array 
+//push..
+const itemList = ["Burger", " fries", "pizza", "spagatti"];
+console.log(itemList.push("macroni"));
+console.log(itemList);
+//pop
+const itemLists = [" Pink ", "Blue ", "Orange"];
+itemLists.pop();
+console.log(itemLists);
+//itemLists = itemLists.length()
+console.log(itemLists);
+//
+//Tuples
+let ourTuple;
+ourTuple = [25, false, "this is a coder"];
+console.log(ourTuple);
+console.log("---------------");
+//Tuple
+let our_Tuple;
+our_Tuple = [15, true, "its a program"];
+console.log(our_Tuple);
--- main.ts
+++ main.ts
@@ -1,17 +1,31 @@
-//if else
-//kal class huogi..
-const isRainy:boolean  = false;
-if (isRainy){
-    console.log("Monday class is Off");
-}else{
-    console.log("there is no off");
-}
-console.log("--------------------------------------")
-
-const isCarPunctured :string = "No";
-if (isCarPunctured === "Yes"){
-    console.log("I will be late for the class");
-}else{
-    console.log("I will be on the time");
-}
-//if /elseif /else
+//Array 
+const shoppingLists :string[] =["Bread" , 'Milk' , 'Butter' ];
+console.log(shoppingLists);
+console.log("-----------------");
+const shoppingList  =["Bread" , 'Milk' , 'Butter' ,3,5];
+console.log(shoppingList);
+console.log("-----------------");
+//uniontype
+const item :(string|number)[]=["rose" , "tulip" , "jasmine" ,  2 , 4 ]
+console.log(item)
+//Methods of array 
+//push..
+const itemList : string [] =["Burger" , " fries" ,"pizza" ,"spagatti"];
+console.log(itemList.push("macroni"));
+console.log(itemList)
+//pop
+const itemLists :string [] =[ " Pink " ,"Blue " ,"Orange"]
+itemLists.pop();
+console.log(itemLists)
+//itemLists = itemLists.length()
+console.log(itemLists)
+//
+//Tuples
+let ourTuple : [number , boolean , string];
+ ourTuple = [ 25 , false , "this is a coder"] ;
+console.log (ourTuple);
+console.log("---------------")
+//Tuple
+let our_Tuple : [number , boolean ,string];
+our_Tuple =[15 , true , "its a program"];
+console.log(our_Tuple)
