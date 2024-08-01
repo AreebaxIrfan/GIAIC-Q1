6c59bb9bf305f902ab95c9bfcffc5ade949e8bd1
Areeba Irfan <161799302+Areeba-irfan-Github-account@users.noreply.github.com>
2024-08-01 23:03:38 +0500

    Class-3
    This repository includes the Class 3 code that demonstrates the use of variables in Typescript

---
+++ variable_with_var.js
@@ -0,0 +1,9 @@
+//Today learn about the Variable...11 _Marach _2024
+//Variable means changable.. variable is used for the store any things..
+//variable with var (variable = keyword);
+var myName = "Areeba Irfan";
+var myMsg = "Hi! my name is isfhan ahmed";
+var mySon = "musa";
+var mySon = "musa";
+mySon = "haroon";
+console.log(mySon);
