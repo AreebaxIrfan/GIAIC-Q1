046148b9e5be9c2533a5e1369c3052682b146a7e
Areeba Irfan <161799302+Areeba-irfan-Github-account@users.noreply.github.com>
2024-08-01 23:16:29 +0500

    Class_8

--- main.js
+++ main.js
@@ -1,21 +1,80 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-//Loops 
-for (let i = 0; i < 10; i++) {
-    console.log("areeba Irfan");
+//class 09
+//function 
+//basic synthex of the function 
+//function functionName (parameter1 ,parameter2 ,parameter 3){
+//functionbody
+//}
+function myInfo() {
+    //functionBody
+    console.log("Hi I'm a student");
+    console.log("i'm a coder");
+    console.log("---------------------------");
 }
-;
-console.log("______________");
-for (let i = 5; i > 0; i++) {
-    console.log(i);
+//call the function first otherwise can't be excute the function
+myInfo(); //invoke function 
+myInfo();
+myInfo();
+console.log("-----------------");
+//function with parameter
+function myfunctionInParameter(name, country, job) {
+    console.log("This is ", name);
+    console.log(`I'm from ${country}`);
+    console.log("I am a" + job);
 }
-console.log("______________");
-//for loop for even number 
-for (let i = 1; i <= 10; i++) {
-    if (i % 2 === 0) {
-        continue;
+myfunctionInParameter('sara ', 'pakistan', 'Software Engineer');
+console.log("+++++++++++++++++++++++++++");
+function myfunctions(name, country, job = 'Software Engineer') {
+    console.log("This is ", name);
+    console.log(`I'm from ${country}`);
+    console.log("I am a" + job);
+}
+myfunctions("ali", "Pakistan");
+console.log("+++++++++++++++++++++++++++");
+function myFunctions(name, country, job) {
+    console.log("Hi my name is ", name);
+    console.log(`i am from ${country}`);
+    if (job) {
+        console.log("I am a" + job);
     }
-    else {
-        console.log(i);
-    }
 }
+myFunctions("Sara", "Korea");
+console.log("+++++++++++++++++++++++++++");
+function myfunctionsSum(n1, n2) {
+    return n1 + n2;
+}
+const result = myfunctionsSum(20, 10);
+console.log(result);
+console.log("+++++++++++++++++++++++++++");
+toDateTime();
+function toDateTime() {
+    const date = new Date();
+    console.log(date);
+}
+console.log("+++++++++++++++++++++++++++");
+const myfunctionIs = function () {
+    console.log("My name is Sara ");
+    console.log("I am from Iceland");
+};
+myfunctionIs();
+//Arrow Function
+const myFunctionsIs = (num1, num2) => num1 * num2;
+const result2 = myFunctionsIs(10, 20);
+console.log(result2);
+console.log("+++++++++++++++++++++++++++");
+const functionsIs = (name, country, job) => {
+    //functionBody
+    console.log("My name is Sara " + name);
+    console.log("I am from Iceland" + country);
+    console.log("I am at a" + job);
+};
+const sumAllNum = (...numbers) => {
+    let total = 0;
+    for (const number of numbers) {
+        total += number;
+    }
+    return total;
+};
+const result3 = sumAllNum(10, 20, 90);
+console.log(result3);
--- main.ts
+++ main.ts
@@ -1,17 +1,87 @@
-//Loops 
-for (let i=0; i < 10 ; i++){
-    console.log("areeba Irfan")
-};
-console.log("______________")
-for( let i = 5 ; i> 0 ; i++){
-    console.log(i)
+//class 09
+//function 
+//basic synthex of the function 
+//function functionName (parameter1 ,parameter2 ,parameter 3){
+    //functionbody
+//}
+function myInfo (): void{ //function declaration
+    //functionBody
+    console.log("Hi I'm a student");
+    console.log("i'm a coder")
+    console.log("---------------------------")
 }
-console.log("______________")
-//for loop for even number 
-for (let i=1 ; i<= 10 ; i++){
-    if (i % 2 === 0){
-        continue;
-    }else {
-        console.log(i)
+//call the function first otherwise can't be excute the function
+myInfo (); //invoke function 
+myInfo ();
+myInfo ();
+console.log("-----------------");
+//function with parameter
+function myfunctionInParameter (name:string , country :string , job :string):void{
+    console.log("This is ", name);
+    console.log(`I'm from ${country}`);
+    console.log("I am a" + job)
+}
+myfunctionInParameter('sara ' , 'pakistan' , 'Software Engineer');
+
+console.log("+++++++++++++++++++++++++++");
+function myfunctions(name :string , country :string , job:string = 'Software Engineer' ){
+    console.log("This is ", name);
+    console.log(`I'm from ${country}`);
+    console.log("I am a" + job)
+}
+
+myfunctions("ali" ,"Pakistan")
+console.log("+++++++++++++++++++++++++++");
+function myFunctions(name :string , country :string ,job?:string):void{
+    console.log("Hi my name is " ,name);
+    console.log(`i am from ${country}`);
+    if (job){
+        console.log("I am a" + job);
     }
+  
 }
+myFunctions("Sara" , "Korea");
+console.log("+++++++++++++++++++++++++++");
+function myfunctionsSum ( n1 :number , n2 :number):number{
+    return n1+ n2
+}
+const result :number = myfunctionsSum( 20 , 10);
+console.log(result)
+console.log("+++++++++++++++++++++++++++");
+
+toDateTime();
+
+function toDateTime(): void{
+    const date : Date =new Date();
+    console.log(date)
+}
+console.log("+++++++++++++++++++++++++++");
+const myfunctionIs = function () :void {
+    console.log("My name is Sara ");
+    console.log("I am from Iceland");
+
+};
+myfunctionIs();
+
+//Arrow Function
+const myFunctionsIs = (num1 :number ,num2 :number) => num1 * num2;
+const result2 : number = myFunctionsIs (10,20);
+console.log(result2)
+console.log("+++++++++++++++++++++++++++");
+const functionsIs = (name : string , country :string , job :string)  => {
+    //functionBody
+    console.log("My name is Sara " +name);
+    console.log("I am from Iceland" +country);
+    console.log("I am at a" + job);
+};
+
+const sumAllNum = (...numbers :number[]) :number => {
+    let total :number = 0;
+    for( const number of numbers) {
+        total += number;
+
+    }
+    return total; 
+}
+const result3 :number =  sumAllNum (10,20,90 );
+console.log(result3)
