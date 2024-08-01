735a19c24a2b8abfded65f77c31e8e4c9c614264
Areeba Irfan <161799302+Areeba-irfan-Github-account@users.noreply.github.com>
2024-08-01 23:15:05 +0500

    Class_7

--- main.js
+++ main.js
@@ -1,33 +1,21 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-//Array 
-const shoppingLists = ["Bread", 'Milk', 'Butter'];
-console.log(shoppingLists);
-console.log("-----------------");
-const shoppingList = ["Bread", 'Milk', 'Butter', 3, 5];
-console.log(shoppingList);
-console.log("-----------------");
-//uniontype
-const item = ["rose", "tulip", "jasmine", 2, 4];
-console.log(item);
-//Methods of array 
-//push..
-const itemList = ["Burger", " fries", "pizza", "spagatti"];
-console.log(itemList.push("macroni"));
-console.log(itemList);
-//pop
-const itemLists = [" Pink ", "Blue ", "Orange"];
-itemLists.pop();
-console.log(itemLists);
-//itemLists = itemLists.length()
-console.log(itemLists);
-//
-//Tuples
-let ourTuple;
-ourTuple = [25, false, "this is a coder"];
-console.log(ourTuple);
-console.log("---------------");
-//Tuple
-let our_Tuple;
-our_Tuple = [15, true, "its a program"];
-console.log(our_Tuple);
+//Loops 
+for (let i = 0; i < 10; i++) {
+    console.log("areeba Irfan");
+}
+;
+console.log("______________");
+for (let i = 5; i > 0; i++) {
+    console.log(i);
+}
+console.log("______________");
+//for loop for even number 
+for (let i = 1; i <= 10; i++) {
+    if (i % 2 === 0) {
+        continue;
+    }
+    else {
+        console.log(i);
+    }
+}
--- main.ts
+++ main.ts
@@ -1,31 +1,17 @@
-//Array 
-const shoppingLists :string[] =["Bread" , 'Milk' , 'Butter' ];
-console.log(shoppingLists);
-console.log("-----------------");
-const shoppingList  =["Bread" , 'Milk' , 'Butter' ,3,5];
-console.log(shoppingList);
-console.log("-----------------");
-//uniontype
-const item :(string|number)[]=["rose" , "tulip" , "jasmine" ,  2 , 4 ]
-console.log(item)
-//Methods of array 
-//push..
-const itemList : string [] =["Burger" , " fries" ,"pizza" ,"spagatti"];
-console.log(itemList.push("macroni"));
-console.log(itemList)
-//pop
-const itemLists :string [] =[ " Pink " ,"Blue " ,"Orange"]
-itemLists.pop();
-console.log(itemLists)
-//itemLists = itemLists.length()
-console.log(itemLists)
-//
-//Tuples
-let ourTuple : [number , boolean , string];
- ourTuple = [ 25 , false , "this is a coder"] ;
-console.log (ourTuple);
-console.log("---------------")
-//Tuple
-let our_Tuple : [number , boolean ,string];
-our_Tuple =[15 , true , "its a program"];
-console.log(our_Tuple)
+//Loops 
+for (let i=0; i < 10 ; i++){
+    console.log("areeba Irfan")
+};
+console.log("______________")
+for( let i = 5 ; i> 0 ; i++){
+    console.log(i)
+}
+console.log("______________")
+//for loop for even number 
+for (let i=1 ; i<= 10 ; i++){
+    if (i % 2 === 0){
+        continue;
+    }else {
+        console.log(i)
+    }
+}
