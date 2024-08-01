97c41ac0fcece81a50b48e0953326202f14440bc
Areeba Irfan <161799302+Areeba-irfan-Github-account@users.noreply.github.com>
2024-08-01 23:13:02 +0500

    Class_4

--- main.js
+++ main.js
@@ -1,20 +1,46 @@
-"use strict";
-//DATA TYPE//TS 
-//permitive data type
-let myName = "Areeba Irfan";
-// myName = 240;///infer
-myName = "The Cat";
-let myAge = 17;
-// myAge = "dist" //infer
-myAge = 35.2466;
-console.log(myAge.toFixed(2)); //
-// let isJobless: boolean = false;
-// // isJobless = 35  ///infere data ko dekh kar data type karta ha..
-// isJobless = true;
-console.log(myName.toUpperCase()); //THE WOLF
-//undefined
-let myplan;
-console.log(myplan); //undefined
-//null
-let myCar = null;
-console.log(myCar);
+//operator in typescript.. (25 march 2024).
+//operators allow us to perform operations on variable and values.
+//Arthematic operator.
+var add = 2 + 4;
+console.log(add);
+var sub = 3 - 3;
+console.log(sub);
+//power 
+var power = Math.pow(2, 4);
+console.log(power);
+//assigment operator..
+//post peri
+var x = 10;
+x += 5;
+console.log(x);
+//by sir isfhan
+var pizzaprice = 1000; //long way code
+var tax = 150;
+var total = pizzaprice + tax;
+console.log(total);
+var pizzaprices = 1000;
+var taxs = 150;
+pizzaprices += taxs;
+console.log("Pizza Prices =", pizzaprices);
+//Comperison Method..(Operator)
+var dataBasePassword = "12345"; //when we store this password
+var resentlyusedthepassword = 12345; //"123456"
+console.log(dataBasePassword == resentlyusedthepassword);
+console.log(dataBasePassword === resentlyusedthepassword);
+//not equal
+var password = "12345";
+var confirmpassword = "12345";
+console.log('line no 40', password != password);
+console.log('line no 41', password !== password);
+//Comparison..
+var text1 = "A";
+var text2 = "B";
+var result = text1 > text2;
+console.log(result);
+var answer = "correct";
+if (answer === "correct") {
+    console.log("you get the correct");
+}
+else {
+    console.log("you are wrong");
+}
--- main.ts
+++ main.ts
@@ -1,23 +1,55 @@
-//DATA TYPE//TS 
+//operator in typescript.. (25 march 2024).
+//operators allow us to perform operations on variable and values.
+//Arthematic operator.
+let add : number = 2+4;
+console.log(add);
 
-//permitive data type
-let myName:string = "Areeba Irfan";
-// myName = 240;///infer
-myName ="The Cat";
+let sub: number = 3 - 3;
+console.log(sub);
 
-let myAge: number  = 17;
-// myAge = "dist" //infer
-myAge = 35.2466;
-console.log(myAge.toFixed(2)); //round off the number ..
+//power 
+let power : number = 2**4;
+console.log(power)
 
-// let isJobless: boolean = false;
-// // isJobless = 35  ///infere data ko dekh kar data type karta ha..
-// isJobless = true;
+//assigment operator..
+//post peri
+let x = 10;
+x += 5;
+console.log(x);
+//by sir isfhan
 
-console.log ( myName.toUpperCase()); //THE WOLF
-//undefined
-let myplan;
-console.log(myplan); //undefined
- //null
- let myCar: null = null
- console.log(myCar)
+let pizzaprice : number =1000;//long way code
+let tax =150;
+let total = pizzaprice + tax;
+console.log(total);
+
+let pizzaprices: number =1000;
+let taxs :number =150;
+pizzaprices += taxs;
+console.log("Pizza Prices =", pizzaprices );
+
+//Comperison Method..(Operator)
+let dataBasePassword : string ="12345"; //when we store this password
+let resentlyusedthepassword : any = 12345; //"123456"
+console.log(dataBasePassword == resentlyusedthepassword );
+console.log(dataBasePassword === resentlyusedthepassword );
+
+//not equal
+let password :string ="12345";
+let confirmpassword :string ="12345";
+console.log('line no 40', password!=password);
+console.log('line no 41' , password !== password);
+//Comparison..
+let text1 ="A";
+let text2 ="B";
+let result = text1 > text2;
+console.log(result);
+
+let answer :string ="correct";
+if (answer === "correct"){
+    console.log("you get the correct")
+}else {
+    console.log("you are wrong")
+}
+
+
