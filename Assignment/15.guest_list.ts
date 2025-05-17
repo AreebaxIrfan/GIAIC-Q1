8ae593de02b82dd127bd5894dd2720c035c57197
Areeba Irfan <161799302+AreebaxIrfan@users.noreply.github.com>
2025-05-17 23:48:55 +0500

    Update 15.guest_list.ts

--- Assignment/15.guest_list.ts
+++ Assignment/15.guest_list.ts
@@ -3,13 +3,6 @@
 
 let guestName : string [] = ["Ali" , "Bilal" , "Umar" , "Hamza"];
 
-// console.log(guestName [0]);
-// console.log(guestName [1]);
-// console.log(guestName [2]);
-// console.log(guestName [3]);
-
-// guestName.map((items) => console.log(`hello! ${items}, I am very glad to invite you to in a dinner`));
-
 let absent_Guest : string = "Ali" ;
 let new_Guest : string = "Shahmeer" ;
 
