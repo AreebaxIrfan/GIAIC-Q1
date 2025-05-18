a40a50bcf3d7a467a61f21557e273333cf0e6cc1
Areeba Irfan <161799302+AreebaxIrfan@users.noreply.github.com>
2025-05-18 22:43:56 +0500

    Update 16.guest_list.ts

--- Assignment/16.guest_list.ts
+++ Assignment/16.guest_list.ts
@@ -3,28 +3,10 @@
 
 
 let guestName : string [] = ["Ali" , "Bilal" , "Umar" , "Hamza"];
-
-// console.log(guestName [0]);
-// console.log(guestName [1]);
-// console.log(guestName [2]);
-// console.log(guestName [3]);
-
-// guestName.map((items) => console.log(`hello! ${items}, I am very glad to invite you to in a dinner`));
-
 let absent_Guest : string = "Ali" ; //Mr.ali is not coming
 let new_Guest : string = "Shahmeer" ; // the new guest is Shahmeer..
 
 guestName [0] = new_Guest
-// guestName.map((items) => console.log(`hello! ${items}, I am very glad to invite you to in a dinner`));
-
-// //this is  guest is not coming head line
-// console.log(`Mr ${absent_Guest} is not coming to the dinner`)
-
-//Good news.....
-
-//Task number 16 coding starting..
-
-
 console.log('Big News! We find big table so we invite more 3 friends');
 
 //the name pf new guest is..
