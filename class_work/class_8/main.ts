4d776eaa2786d5b4168493110cf77153a93335a9
Areeba Irfan <161799302+AreebaxIrfan@users.noreply.github.com>
2025-06-06 17:41:50 +0500

    Update main.ts

--- class_work/class_8/main.ts
+++ class_work/class_8/main.ts
@@ -3,9 +3,7 @@ for (let i=0; i < 10 ; i++){
     console.log("areeba Irfan")
 };
 console.log("______________")
-for( let i = 5 ; i> 0 ; i++){
-    console.log(i)
-}
+
 console.log("______________")
 //for loop for even number 
 for (let i=1 ; i<= 10 ; i++){
