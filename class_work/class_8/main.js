6bf9c52a40e92f12ea36ab6e6f5079405c48a4f5
Areeba Irfan <161799302+AreebaxIrfan@users.noreply.github.com>
2025-06-07 15:00:56 +0500

    Update main.js

--- class_work/class_8/main.js
+++ class_work/class_8/main.js
@@ -6,11 +6,6 @@ for (let i = 0; i < 10; i++) {
 }
 ;
 console.log("______________");
-for (let i = 5; i > 0; i++) {
-    console.log(i);
-}
-console.log("______________");
-//for loop for even number 
 for (let i = 1; i <= 10; i++) {
     if (i % 2 === 0) {
         continue;
