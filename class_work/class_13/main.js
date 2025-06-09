b8d9a19cec2d5c954dc8404fa6cb95e857cbc2fa
Areeba Irfan <161799302+AreebaxIrfan@users.noreply.github.com>
2025-06-09 14:09:00 +0500

    Update main.js

--- class_work/class_13/main.js
+++ class_work/class_13/main.js
@@ -1,34 +1,28 @@
-var pubg;
-pubg = " I am playing PUBG ";
+let pubg;
+pubg = "I am playing PUBG";
 console.log(pubg);
-//SYNTRONUS 
+
+// SYNCHRONOUS
 function makePizza() {
-    var order = "Your pizza  is being prepared";
-    console.log(order, "Kitchen"); //function local scope
+    const order = "Your pizza is being prepared";
+    console.log(order, "Kitchen"); // function local scope
     return order;
 }
-;
-var deliver = makePizza();
+
+const deliver = makePizza();
 console.log(deliver, "Table");
-//Promise
+
+// PROMISE example
 function makePizza1() {
     console.log("Your Pizza is being prepared");
 }
-// let sirZiaPromise1 = new Promise (() => "Your Pizza is being prepared");
-// console.log(new Promise () ," Table") 
-// let sirZiaPromise =  make Promise (() => "Your Pizza is being prepared");
-// console.log(make Promise () ,"In Table") 
-// let sirPromise = new Promise ((resolve ; reject)=>{console.log"it's time to off"}){
-//     if (new Promise = "It's time to off"){
-//         console.log("")
-//     }else{
-//         console.log("NOPE")
-//     }
-// }
-var sirZia = new Promise(function (resolve, reject) {
+
+// Correct promise example
+const sirZia = new Promise(function (resolve, reject) {
     console.log("Its time to off");
     resolve("True");
 });
+
 sirZia.then(function () {
     console.log("True");
 });
