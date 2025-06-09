ac37608adb0c6ac3924f200d368b8e5157fb5480
Areeba Irfan <161799302+AreebaxIrfan@users.noreply.github.com>
2025-06-09 14:33:06 +0500

    Update main.ts

--- class_work/class_11/chalk/main.ts
+++ class_work/class_11/chalk/main.ts
@@ -9,13 +9,3 @@ console.log(
 );
 console.log(chalk.blueBright.bold.bgGray('Ali'));
 console.log(chalk.red.bgYellow('Hello' , chalk.underline.bgBlueBright.yellow('world') + '!'))
-// import inquirer from "inquirer";
-// import chalk from "chalk";
-
-// let answers = await inquirer.prompt([{
-//     name: "age",
-//     type: "number",
-//     message: "Enter your Age:"}
-// ]);
-
-// console.log(chalk.blue("Insha Allah, in " + (60 - answers.age) + " years you will be 60 years old."));
