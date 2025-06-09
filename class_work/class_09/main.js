c2564a55abcee584a70b1473058f613d516f3744
Areeba Irfan <161799302+AreebaxIrfan@users.noreply.github.com>
2025-06-09 14:28:20 +0500

    Update main.js

--- class_work/class_09/main.js
+++ class_work/class_09/main.js
@@ -1,74 +1,95 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-//class 09
-//function 
-//basic synthex of the function 
-//function functionName (parameter1 ,parameter2 ,parameter 3){
-//functionbody
-//}
+
+// class 09
+// function basic syntax
+
 function myInfo() {
-    //functionBody
+    // function body
     console.log("Hi I'm a student");
-    console.log("i'm a coder");
+    console.log("I'm a coder");
     console.log("---------------------------");
 }
-//call the function first otherwise can't be excute the function
-myInfo(); //invoke function 
+// call the function first otherwise can't execute the function
+myInfo(); // invoke function 
 myInfo();
 myInfo();
+
 console.log("-----------------");
-//function with parameter
+
+// function with parameters
 function myfunctionInParameter(name, country, job) {
     console.log("This is ", name);
     console.log(`I'm from ${country}`);
     console.log("I am a" + job);
 }
 myfunctionInParameter('sara ', 'pakistan', 'Software Engineer');
+
 console.log("+++++++++++++++++++++++++++");
+
+// Updated function with different implementation
 function myfunctions(name, country, job = 'Software Engineer') {
-    console.log("This is ", name);
-    console.log(`I'm from ${country}`);
-    console.log("I am a" + job);
+    if (!name || !country) {
+        console.log("Name and country are required!");
+        return;
+    }
+    console.log(`Name: ${name.toUpperCase()}`);  // show name uppercase
+    console.log(`Country: ${country.charAt(0).toUpperCase() + country.slice(1)}`); // Capitalize first letter
+    console.log(`Occupation: ${job.toLowerCase()}`);  // show job in lowercase
 }
-myfunctions("ali", "Pakistan");
+myfunctions("ali", "pakistan");
+
 console.log("+++++++++++++++++++++++++++");
+
 function myFunctions(name, country, job) {
     console.log("Hi my name is ", name);
-    console.log(`i am from ${country}`);
+    console.log(`I am from ${country}`);
     if (job) {
-        console.log("I am a" + job);
+        console.log("I am a " + job);
     }
 }
 myFunctions("Sara", "Korea");
+
 console.log("+++++++++++++++++++++++++++");
+
 function myfunctionsSum(n1, n2) {
     return n1 + n2;
 }
 const result = myfunctionsSum(20, 10);
 console.log(result);
+
 console.log("+++++++++++++++++++++++++++");
+
 toDateTime();
 function toDateTime() {
     const date = new Date();
     console.log(date);
 }
+
 console.log("+++++++++++++++++++++++++++");
+
 const myfunctionIs = function () {
     console.log("My name is Sara ");
     console.log("I am from Iceland");
 };
 myfunctionIs();
-//Arrow Function
+
+console.log("+++++++++++++++++++++++++++");
+
+// Arrow Function
 const myFunctionsIs = (num1, num2) => num1 * num2;
 const result2 = myFunctionsIs(10, 20);
 console.log(result2);
+
 console.log("+++++++++++++++++++++++++++");
+
 const functionsIs = (name, country, job) => {
-    //functionBody
+    // functionBody
     console.log("My name is Sara " + name);
-    console.log("I am from Iceland" + country);
-    console.log("I am at a" + job);
+    console.log("I am from Iceland " + country);
+    console.log("I am at a " + job);
 };
+
 const sumAllNum = (...numbers) => {
     let total = 0;
     for (const number of numbers) {
