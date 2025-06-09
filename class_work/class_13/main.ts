b53c1b965fce86c1465677fac0f8f36de39b5095
Areeba Irfan <161799302+AreebaxIrfan@users.noreply.github.com>
2025-06-09 14:36:04 +0500

    Update main.ts

--- class_work/class_13/main.ts
+++ class_work/class_13/main.ts
@@ -16,19 +16,6 @@ console.log(deliver , "Table");
 function makePizza1() {
     console.log("Your Pizza is being prepared")
 }
-
-// let sirZiaPromise1 = new Promise (() => "Your Pizza is being prepared");
-// console.log(new Promise () ," Table") 
- 
-// let sirZiaPromise =  make Promise (() => "Your Pizza is being prepared");
-// console.log(make Promise () ,"In Table") 
-// let sirPromise = new Promise ((resolve ; reject)=>{console.log"it's time to off"}){
-//     if (new Promise = "It's time to off"){
-//         console.log("")
-//     }else{
-//         console.log("NOPE")
-//     }
-// }
 //Promise
 let sirZia = new Promise ((resolve, reject ) =>{
     console.log("Its time to off")
