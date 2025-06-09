ef91a4e1341f9c3dfd8726df1ee33021d23f387a
Areeba Irfan <161799302+AreebaxIrfan@users.noreply.github.com>
2025-06-09 14:06:29 +0500

    Update main.js

--- class_work/class_10/main.js
+++ class_work/class_10/main.js
@@ -1,29 +1,31 @@
 "use strict";
-//class 10
-//objext.ts
-//its the basic syntex of the object type..
-//const objectName ={
-//key :value;
-// ' :' its called colon
-Object.defineProperty(exports, "__esModule", { value: true });
-//}
-//object defination:
-// the syntex of the brackets (paraenthesis) is called object literal
+// class 10
+// object.ts
+// This is the basic syntax of the object type.
+
+// const objectName = {
+//    key: value
+// }
+// ':' is called colon
+
+// Object definition:
+// The syntax of the brackets (parenthesis) is called object literal
+
 let car = "Mehran";
+
 const mycar = {
     name: "Mehran",
     color: "white",
-    brand: "suzoki",
+    brand: "suzuki",
     year: 2021,
 };
-console.log(mycar); //its show to accessice the properties of the object
+
+console.log(mycar); // Access the properties of the object
 console.log(mycar.brand);
-console.log(mycar["color"]); //another way to excute the code..
-////properties
-//the (name : value ) is called the properties..
-//method
-//a method is a function to store the property..
+console.log(mycar["color"]); // Another way to access properties
+
 console.log("------------------------------");
+
 const person = {
     firstname: "Ali",
     lastname: "Khan",
@@ -31,44 +33,50 @@ const person = {
     country: "America",
     SkinColor: "Black",
     fullName: function () {
-        return `Hello my name is ${this.firstname} ${this.lastname}`; //template literal.
+        return `Hello my name is ${this.firstname} ${this.lastname}`; // template literal
     }
 };
+
 console.log(person.fullName());
+
 console.log("----------------");
+
 const pet = {
     type: 'Cat',
     name: 'tom',
     age: 5,
-    color: ' white',
+    color: 'white',
     SayMeow: function () {
         return 'meow meow';
     }
 };
+
 console.log(pet.SayMeow());
+
 console.log("---------------------");
+
 const sara = {
     firstName: "Kamal",
     lastName: "Ali",
     sayHello: function () {
-        return `Hello ,my name is ${this.firstName}`;
+        return `Hello, my name is ${this.firstName}`;
     }
 };
-//Enum 
-//enum is a special  (class)  that represent aa group of const and unchangable value
-//two parts of the enum and the string
-//its start with the 1 value 
-//synthex
-var Color;
+
+// Enum 
+// Enum is a special class that represents a group of constants (unchangeable values)
+// It starts with the 0 value by default
+
+let Color;
 (function (Color) {
     Color[Color["Red"] = 0] = "Red";
     Color[Color["Green"] = 1] = "Green";
-    Color[Color["voilet"] = 2] = "voilet";
+    Color[Color["Violet"] = 2] = "Violet";
     Color[Color["Black"] = 3] = "Black";
     Color[Color["White"] = 4] = "White";
     Color[Color["Yellow"] = 5] = "Yellow";
     Color[Color["Brown"] = 6] = "Brown";
 })(Color || (Color = {}));
-;
-const myFavoiteColor = Color.Black;
-console.log(myFavoiteColor);
+
+const myFavoriteColor = Color.Black;
+console.log(myFavoriteColor);
