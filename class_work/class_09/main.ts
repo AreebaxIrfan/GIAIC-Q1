513e20a01ab236c42e692adb4dfb23042fefc582
Areeba Irfan <161799302+AreebaxIrfan@users.noreply.github.com>
2025-06-09 14:31:06 +0500

    Update main.ts

--- class_work/class_09/main.ts
+++ class_work/class_09/main.ts
@@ -1,87 +1,103 @@
-//class 09
-//function 
-//basic synthex of the function 
-//function functionName (parameter1 ,parameter2 ,parameter 3){
-    //functionbody
-//}
-function myInfo (): void{ //function declaration
-    //functionBody
+// class 09
+// function 
+// basic syntax of the function 
+// function functionName(parameter1, parameter2, parameter3) {
+//   function body
+// }
+
+function myInfo(): void { // function declaration
+    // functionBody
     console.log("Hi I'm a student");
-    console.log("i'm a coder")
-    console.log("---------------------------")
+    console.log("I'm a coder");
+    console.log("---------------------------");
 }
-//call the function first otherwise can't be excute the function
-myInfo (); //invoke function 
-myInfo ();
-myInfo ();
+
+// call the function first otherwise can't execute the function
+myInfo(); // invoke function 
+myInfo();
+myInfo();
+
 console.log("-----------------");
-//function with parameter
-function myfunctionInParameter (name:string , country :string , job :string):void{
+
+// function with parameters
+function myfunctionInParameter(name: string, country: string, job: string): void {
     console.log("This is ", name);
     console.log(`I'm from ${country}`);
-    console.log("I am a" + job)
+    console.log("I am a " + job);
 }
-myfunctionInParameter('sara ' , 'pakistan' , 'Software Engineer');
+myfunctionInParameter('sara', 'pakistan', 'Software Engineer');
 
 console.log("+++++++++++++++++++++++++++");
-function myfunctions(name :string , country :string , job:string = 'Software Engineer' ){
+
+function myfunctions(name: string, country: string, job: string = 'Software Engineer'): void {
     console.log("This is ", name);
     console.log(`I'm from ${country}`);
-    console.log("I am a" + job)
+    console.log("I am a " + job);
 }
 
-myfunctions("ali" ,"Pakistan")
+myfunctions("ali", "Pakistan");
+
 console.log("+++++++++++++++++++++++++++");
-function myFunctions(name :string , country :string ,job?:string):void{
-    console.log("Hi my name is " ,name);
-    console.log(`i am from ${country}`);
-    if (job){
-        console.log("I am a" + job);
+
+function myFunctions(name: string, country: string, job?: string): void {
+    console.log("Hi my name is ", name);
+    console.log(`I am from ${country}`);
+    if (job) {
+        console.log("I am a " + job);
     }
-  
 }
-myFunctions("Sara" , "Korea");
+
+myFunctions("Sara", "Korea");
+
 console.log("+++++++++++++++++++++++++++");
-function myfunctionsSum ( n1 :number , n2 :number):number{
-    return n1+ n2
+
+function myfunctionsSum(n1: number, n2: number): number {
+    return n1 + n2;
 }
-const result :number = myfunctionsSum( 20 , 10);
-console.log(result)
+
+const result: number = myfunctionsSum(20, 10);
+console.log(result);
+
 console.log("+++++++++++++++++++++++++++");
 
 toDateTime();
 
-function toDateTime(): void{
-    const date : Date =new Date();
-    console.log(date)
+function toDateTime(): void {
+    const date: Date = new Date();
+    console.log(date);
 }
-console.log("+++++++++++++++++++++++++++");
-const myfunctionIs = function () :void {
-    console.log("My name is Sara ");
-    console.log("I am from Iceland");
 
+console.log("+++++++++++++++++++++++++++");
+
+const myfunctionIs = function (): void {
+    console.log("My name is Sara");
+    console.log("I am from Iceland");
 };
 myfunctionIs();
 
-//Arrow Function
-const myFunctionsIs = (num1 :number ,num2 :number) => num1 * num2;
-const result2 : number = myFunctionsIs (10,20);
-console.log(result2)
 console.log("+++++++++++++++++++++++++++");
-const functionsIs = (name : string , country :string , job :string)  => {
-    //functionBody
-    console.log("My name is Sara " +name);
-    console.log("I am from Iceland" +country);
-    console.log("I am at a" + job);
+
+// Arrow Function
+const myFunctionsIs = (num1: number, num2: number): number => num1 * num2;
+const result2: number = myFunctionsIs(10, 20);
+console.log(result2);
+
+console.log("+++++++++++++++++++++++++++");
+
+const functionsIs = (name: string, country: string, job: string): void => {
+    // functionBody
+    console.log("My name is Sara " + name);
+    console.log("I am from Iceland " + country);
+    console.log("I am at a " + job);
 };
 
-const sumAllNum = (...numbers :number[]) :number => {
-    let total :number = 0;
-    for( const number of numbers) {
+const sumAllNum = (...numbers: number[]): number => {
+    let total: number = 0;
+    for (const number of numbers) {
         total += number;
-
     }
-    return total; 
-}
-const result3 :number =  sumAllNum (10,20,90 );
-console.log(result3)
+    return total;
+};
+
+const result3: number = sumAllNum(10, 20, 90);
+console.log(result3);
