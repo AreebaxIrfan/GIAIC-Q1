0667662ea9b1b2171aa0aa6a083a695c0b530b83
Areeba Irfan <161799302+AreebaxIrfan@users.noreply.github.com>
2025-06-09 14:20:05 +0500

    Update variable_with_var.js

--- practice/variable/variable_with_var.js
+++ practice/variable/variable_with_var.js
@@ -1,9 +1,11 @@
-//Today learn about the Variable...11 _Marach _2024
-//Variable means changable.. variable is used for the store any things..
-//variable with var (variable = keyword);
-var myName = "Areeba Irfan";
-var myMsg = "Hi! my name is isfhan ahmed";
-var mySon = "musa";
-var mySon = "musa";
+// Today learn about the Variable...11 March 2024
+// Variable means changeable.. variable is used to store anything..
+// variable with let and const (preferred over var);
+
+const myName = "Areeba Irfan";  // value doesn't change, so use const
+const myMsg = "Hi! my name is isfhan ahmed";  // value doesn't change, so use const
+
+let mySon = "musa";  // this will change, so use let
 mySon = "haroon";
+
 console.log(mySon);
