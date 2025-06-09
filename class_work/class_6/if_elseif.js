df119e058070ca48c3eab240a146f5357774c1a3
Areeba Irfan <161799302+AreebaxIrfan@users.noreply.github.com>
2025-06-09 14:16:01 +0500

    Update if_elseif.js

--- class_work/class_6/if_elseif.js
+++ class_work/class_6/if_elseif.js
@@ -1,12 +1,12 @@
-var isRainy = false;
-var isTherePost = true;
+const isRainy = false;
+const isTherePost = true;
+
 if (isRainy && isTherePost) {
     console.log("There is no Class line no 5");
-}
-else if (isTherePost) {
+} else if (isTherePost) {
     console.log("There is no class line no 10");
-}
-else {
+} else {
     console.log("There is class");
 }
+
 console.log("-------------------------------");
