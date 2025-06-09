edba2553a667b3ebc9bdf4b538de1ffa800c1f4a
Areeba Irfan <161799302+AreebaxIrfan@users.noreply.github.com>
2025-06-09 14:14:28 +0500

    Update main.js

--- class_work/class_5/main.js
+++ class_work/class_5/main.js
@@ -1,44 +1,52 @@
-//operator in typescript.. (25 march 2024).
-//operators allow us to perform operations on variable and values.
-//Arthematic operator.
-var add = 2 + 4;
+// operator in TypeScript.. (25 March 2024).
+// operators allow us to perform operations on variables and values.
+// Arithmetic operator.
+
+const add = 2 + 4;
 console.log(add);
-//power 
-var power = Math.pow(2, 4);
+
+// power 
+const power = Math.pow(2, 4);
 console.log(power);
-//assigment operator..
-//post peri
-var x = 10;
+
+// assignment operator..
+// post increment/decrement example
+let x = 10;
 x += 5;
 console.log(x);
-//by sir isfhan
-var pizzaprice = 1000; //long way code
-var tax = 150;
-var total = pizzaprice + tax;
+
+// by Sir Isfhan
+const pizzaprice = 1000; // long way code
+const tax = 150;
+const total = pizzaprice + tax;
 console.log(total);
-var pizzaprices = 1000;
-var taxs = 150;
+
+let pizzaprices = 1000;
+const taxs = 150;
 pizzaprices += taxs;
 console.log("Pizza Prices =", pizzaprices);
-//Comperison Method..(Operator)
-var dataBasePassword = "12345"; //when we store this password
-var resentlyusedthepassword = 12345; //"123456"
+
+// Comparison Method..(Operator)
+const dataBasePassword = "12345"; // when we store this password
+const resentlyusedthepassword = 12345; //"123456"
 console.log(dataBasePassword == resentlyusedthepassword);
 console.log(dataBasePassword === resentlyusedthepassword);
-//not equal
-var password = "12345";
-var confirmpassword = "12345";
+
+// not equal
+const password = "12345";
+const confirmpassword = "12345";
 console.log('line no 40', password != password);
 console.log('line no 41', password !== password);
-//Comparison..
-var text1 = "A";
-var text2 = "B";
-var result = text1 > text2;
+
+// Comparison..
+const text1 = "A";
+const text2 = "B";
+const result = text1 > text2;
 console.log(result);
-var answer = "correct";
+
+const answer = "correct";
 if (answer === "correct") {
     console.log("you get the correct");
-}
-else {
+} else {
     console.log("you are wrong");
 }
