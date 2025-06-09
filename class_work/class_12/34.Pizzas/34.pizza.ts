175b0deaed933df1956be3b5addc8237a7698100
Areeba Irfan <161799302+AreebaxIrfan@users.noreply.github.com>
2025-06-09 14:35:21 +0500

    Update 34.pizza.ts

--- class_work/class_12/34.Pizzas/34.pizza.ts
+++ class_work/class_12/34.Pizzas/34.pizza.ts
@@ -1,13 +1,14 @@
-//Task number # 34
-//Think of at least three kinds of your favorite pizza. Store these pizza names in a array, and then use a for loop to print the name of each pizza.
+// Task number #34
+// Think of at least three kinds of your favorite pizza. Store these pizza names in an array, 
+// and then use a for loop to print the name of each pizza.
 
-let favorite_pizza : string [] = ["Chiken" , "veg" , "cheese" , "pepperoni "]
+let favorite_pizza: string[] = ["Chicken", "Veg", "Cheese", "Pepperoni"];
 
 for (let pizza of favorite_pizza) {
-    console.log(pizza)
+    console.log(pizza);
 }
-console.log("\n")
+console.log("\n");
 
-for(let pizza of favorite_pizza){
-    console.log("I really Like ${pizza} pizza!")
+for (let pizza of favorite_pizza) {
+    console.log(`I really like ${pizza} pizza!`);
 }
