9cb0025bbc1a66ae00b16cd6f497c43a9f0e2a44
Areeba Irfan <161799302+AreebaxIrfan@users.noreply.github.com>
2025-06-09 14:33:43 +0500

    Update main.js

--- class_work/class_11/module_work/main.js
+++ class_work/class_11/module_work/main.js
@@ -1,8 +1,4 @@
-//modules
-// import a from "./first";
-// console.log(a);
-// import {job} from "./first";
-// console.log(job);
+
 import x, { job } from "./first.js";
 console.log(x, job);
 import { add } from "./second.js";
