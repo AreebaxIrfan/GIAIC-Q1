95d658c9accacc6d7f9fc3d91928f2bbb6e90f90
Areeba Irfan <161799302+AreebaxIrfan@users.noreply.github.com>
2025-06-09 14:11:56 +0500

    Update main.js

--- class_work/class_14/main.js
+++ class_work/class_14/main.js
@@ -1,29 +1,18 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-///promise
-let sirziakhan = new Promise(function (response, reject) {
-    //    response('Passed')
+
+// Promise that rejects immediately
+const sirziakhan = new Promise(function (resolve, reject) {
+    // resolve('Passed'); // Uncomment if you want to resolve
     reject('failed');
 });
-function promiseStatus() {
-    return __awaiter(this, void 0, void 0, function* () {
-        try {
-            let response = yield sirziakhan;
-            console.log(response, " response");
-        }
-        catch (error) {
-            console.log(error, "Error");
-        }
-        // let response = await sirziakhan
-        // console.log(response)
-    });
+
+async function promiseStatus() {
+    try {
+        const response = await sirziakhan;
+        console.log(response, "response");
+    } catch (error) {
+        console.log(error, "Error");
+    }
 }
+
 promiseStatus();
