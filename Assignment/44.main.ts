468a0639377a45825dca03b0d2315fa9d2406936
Areeba Irfan <161799302+AreebaxIrfan@users.noreply.github.com>
2025-08-05 12:02:29 +0500

    Update 44.main.ts

--- Assignment/44.main.ts
+++ Assignment/44.main.ts
@@ -1,14 +1,15 @@
-//Task number 44
-//Write a function that accepts a array of items a person wants on a sandwich. The function should have one parameter that collects as many items as the function call provides, and it should print a summary of the sandwich that is being ordered. Call the function three times, using a different number of arguments each time.
+"use strict";
+// Task number 44
+// Write a function that accepts an array of items a person wants on a sandwich. The function should have one parameter that collects as many items as the function call provides, and it should print a summary of the sandwich that is being ordered. Call the function three times, using a different number of arguments each time.
 
 function sandwich(...items: string[]): void {
-    console.log("Sandwich order:  ")
-    for(let i = 0; i < items.length; i++){
-        console.log(` - ${items[i]}`)
+    console.log("Sandwich order:");
+    for (const item of items) {
+        console.log(` - ${item}`);
     }
 }
-console.log("enjoy your sandwich Eric")
 
-sandwich('capsium' , 'tomato' , 'chicken')
-sandwich('beef' , 'cheese')
-sandwich('garlic chicken' , 'mayo sauce')
+console.log("Enjoy your sandwich, Eric!");
+sandwich('capsicum', 'tomato', 'chicken');
+sandwich('beef', 'cheese');
+sandwich('garlic chicken', 'mayo sauce');
