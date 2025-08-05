5cbe26e27584210346ace73d5c268a85acba13ec
Areeba Irfan <161799302+AreebaxIrfan@users.noreply.github.com>
2025-08-05 12:04:11 +0500

    Update 45.car.js

--- Assignment/45.car.js
+++ Assignment/45.car.js
@@ -1,9 +1,8 @@
 "use strict";
-//Task number 45
-function creatCar(manufacture, model, optional) {
-    return Object.assign({ manufacture,
-        model }, optional);
+// Task number 45
+function creatCar(manufacture: string, model: string, optional: { [key: string]: any }): { manufacture: string, model: string, [key: string]: any } {
+    return { manufacture, model, ...optional };
 }
-;
+
 const mycar = creatCar("toyota", "corolla", { color: "silver", year: "2024" });
 console.log(mycar);
