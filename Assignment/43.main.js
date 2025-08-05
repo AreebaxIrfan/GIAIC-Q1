bcb2d9ee1c036d794c78d4e01a9efb7b00ceff2d
Areeba Irfan <161799302+AreebaxIrfan@users.noreply.github.com>
2025-08-05 11:59:13 +0500

    Update 43.main.js

--- Assignment/43.main.js
+++ Assignment/43.main.js
@@ -1,28 +1,31 @@
 "use strict";
-//Task number #43
-//Unchanged Magicians: Start with your work from Exercise 40. Call the function make_great() with a copy of the array of magicians’ names. Because the original array will be unchanged, return the new array and store it in a separate array. Call show_magicians() with each array to show that you have one array of the original names and one array with the Great added to each magician’s name.
+// Task number #43
+// Unchanged Magicians: Start with your work from Exercise 40. Call the function make_great() with a copy of the array of magicians’ names. Because the original array will be unchanged, return the new array and store it in a separate array. Call show_magicians() with each array to show that you have one array of the original names and one array with the Great added to each magician’s name.
+
 function show_magicians(magicians) {
     for (const magician of magicians) {
         console.log(magician.charAt(0).toUpperCase() + magician.slice(1));
     }
 }
-// const magician: string[] =["Bilal" ,"Ali" , "Irfan" , "Maaz" , "Saad"]
-// show_magicians(magician)
+
 function make_great(magicians) {
     for (let i = 0; i < magicians.length; i++) {
         magicians[i] = magicians[i] + ' the great';
     }
 }
-const magicians2 = ["Bilal", "Ali", "Irfan", "Maaz", "Saad"];
-make_great(magicians2);
-show_magicians(magicians2);
+
 function make_great2(magicians) {
     const greatMagicians = [];
-    for (let i = 0; i < magicians.length; i++) {
-        greatMagicians.push(magicians[i] + ' the Great');
+    for (const magician of magicians) {
+        greatMagicians.push(magician + ' the Great');
     }
     return greatMagicians;
 }
+
+const magicians2 = ["Bilal", "Ali", "Irfan", "Maaz", "Saad"];
+make_great(magicians2);
+show_magicians(magicians2);
+
 const magicians3 = ["Ali", "Bilal", "Irfan", "Maaz"];
 const greatMagicians2 = make_great2(magicians3);
 show_magicians(magicians3);
